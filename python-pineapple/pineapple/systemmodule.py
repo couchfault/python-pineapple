2fc307ac283908c8591a6198589452f2bc008520
Henry Pitcairn <735Tesla@gmail.com>
2015-11-22 14:32:29 -0500

    delete usueless file

--- python-pineapple/pineapple/systemmodule.py
+++
@@ -1,7 +0,0 @@
-from api import Pineapple
-
-class SystemModule(object):
-    def __init__(self, pineapple, name):
-        super(SystemModule, self).__init__()
-        self.pineapple = pineapple
-    def request(self)
